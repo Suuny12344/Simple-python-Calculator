1ba510159ac4183baa34633c9056b84b16110c93
Suuny12344 <julpoh15@gmail.com>
2026-09-06 15:44:45 +0200

    Fixed the rounding

--- Calculator.py
+++ Calculator.py
@@ -7,22 +7,22 @@ num2 = float(input("Enter the 2nd number: "))
 
 if operator == "+":
     result = num1 + num2
-    print(round(result, 3))
+    print(round(result, 2))
 elif operator == "-":
     result = num1 - num2
-    print(round(result, 3))
+    print(round(result, 2))
 elif operator == "*":
     result = num1 * num2
-    print(round(result, 3))
+    print(round(result, 2))
 elif operator == "/":
     result = num1 / num2
-    print(round(result, 3))
+    print(round(result, 2))
 elif operator == "**":
     result = num1 ** num2
-    print(round(result, 3))
+    print(round(result, 2))
 elif operator == "%":
     result = num1 % num2
-    print(round(result, 3))
+    print(round(result, 2))
 elif operator == "//":
     result = num1 // num2
     print(int(result))
